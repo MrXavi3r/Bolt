c4084da9276567ecb87533094e697fed7b34e5ca
Xavier Ritch <xritch89@hotmail.com>
2020-08-02 21:03:26 -0400

    card, background, header & account services going

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,33 +6,65 @@ import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
 
 
-
-const useStyles = makeStyles((theme) => ({
+//applies additional styling to elements in Login 
+const useStyles = makeStyles({
     root: {
       '& > *': {
-        margin: theme.spacing(1),
-        width: '25ch',
-      },
+        display: 'flex',
+        margin: '10px',
+     }, 
     },
-  }));
+    card: {
+      padding: '40px',
+    },
+    div: {
+      padding: '80px',
+      backgroundColor: '#1652f0'
+    },
+    h4: {
+      color: 'white',
+      fontFamily: 'helvetica',
+    },
+    //not working for some reason 
+    account: {
+      color: 'white',
+    }
+  });
 
+
+//Login Page 
 export default function Login() {
     const classes = useStyles();
 
     return (
-      <div>
-        <Card raised="true" className={classes.root} variant="outlined">
-          <form className={classes.root} noValidate autoComplete="off">
-            <TextField type="text" id="username-input" label="username" variant="outlined" required />
-            <TextField type="password" id="password-input" label="password" variant="outlined" required />
-            <FormControlLabel control={<Checkbox name="checkedC" />} color="primary" label="keep me signed in on this computer" />
-            <CardActions>
-              <Button variant="primary">Login</Button>
-            </CardActions>
-          </form>
-        </Card>
-      </div>
+    <div className={classes.div}>
+      <Grid container direction="column" alignItems="center">
+      {/* Using Box component to act as a wrapper to apply bootstrap spacing */}
+        <Box pb={3}>
+          <Typography variant='h4' className={classes.h4}>Login To BOLT</Typography>
+        </Box>
+          <Card raised="true" className={classes.card} variant="outlined">
+            <form className={classes.root} noValidate autoComplete="off">
+              <TextField type="text" id="username-input" label="username" variant="outlined" required error={false} />
+              <TextField type="password" id="password-input" label="password" variant="outlined" required error={false} />
+              <FormControlLabel control={<Checkbox name="checkedA" />} label="keep me logged in on this computer" />
+              <CardActions>
+                <Box mx={'auto'}>
+                  <Button color="primary" variant="outlined" size="large">Login</Button>
+                </Box>
+              </CardActions>
+            </form>
+          </Card>
+          <Box pt={2}>
+            <Typography classes={classes.account} variant='p'><Link href="#">Forgot password?</Link> <Link href="#">Don't have an account?</Link> <Link href="#">Privacy Policy</Link></Typography>
+          </Box>
+      </Grid>
+    </div>
     );
 }
